57491218ce6f5f149e33ca51d4d35e947228b133
elb00507 <elb00507@g.bstu.by>
2025-05-16 23:23:30 +0300

    feat: add burger menu
    Добавлено меню на разрешении 598px

--- src/Common/Header.ts
+++ src/Common/Header.ts
@@ -1,6 +1,9 @@
 import { Component } from '../Abstract/Component';
 
 export class Header extends Component {
+	private menuIcon: Component;
+	private mobileMenu: HTMLElement;
+	private mobileWrapper: HTMLElement;
 	constructor(parent: HTMLElement) {
 		super(parent, 'header', ['header']);
 
@@ -9,6 +12,32 @@ export class Header extends Component {
 		new Component(headerCont.root, 'a', ['logo-link'], 'hairpin');
 
 		const headerNav = new Component(headerCont.root, 'div', ['header__nav']);
+
+		const links = [
+			{ text: 'Shop', href: '#shop' },
+			{ text: 'About', href: '#about' },
+			{ text: 'Cart', href: '#cart' },
+			{ text: 'Sign in', href: '#signin' },
+		];
+
+		this.mobileMenu = new Component(this.root, 'div', [
+			'header__mobile-menu',
+		]).root;
+
+		this.mobileWrapper = new Component(this.mobileMenu, 'div', [
+			'header__mobile-menu-wrap',
+		]).root;
+
+		links.forEach((link) => {
+			const linkComponent = new Component(
+				this.mobileWrapper,
+				'a',
+				['header__mobile-link'],
+				link.text,
+				['href'],
+				[link.href]
+			);
+		});
 
 		new Component(
 			headerNav.root,
@@ -30,7 +59,7 @@ export class Header extends Component {
 		new Component(
 			headerNav.root,
 			'a',
-			['header__link--Elirix'],
+			['header__link'],
 			'Cart',
 			['href'],
 			['#cart']
@@ -44,5 +73,8 @@ export class Header extends Component {
 			['href'],
 			['#signin']
 		);
+
+		this.menuIcon = new Component(this.root, 'div', ['header__menu-icon']);
+		this.menuIcon.root.innerHTML = '<span></span><span></span><span></span>';
 	}
 }
