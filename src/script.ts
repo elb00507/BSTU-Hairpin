148627a961228d837b2b007f9201c17754130a7a
elb00507 <elb00507@g.bstu.by>
2025-05-16 22:05:41 +0300

    feat: add Component
    Добавлен файл компонент и главный объект приложения

--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,16 @@
 import '../src/style.scss';
+import { Component } from './Abstract/Component';
+
+declare global {
+	interface Window {
+		app: App;
+	}
+}
+
+class App {
+	constructor(parent: HTMLElement) {
+		const wrap = new Component(parent, 'div', ['wrapper']);
+	}
+}
+
+window.app = new App(document.body);
