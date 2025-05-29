1c4f9d47b03d6868d9590eea6e04219e5685669a
elb00507 <elb00507@g.bstu.by>
2025-05-29 21:56:35 +0300

    fix: fixing style scss
    Исправлены стили mainpage

--- src/Pages/MainPage.ts
+++ src/Pages/MainPage.ts
@@ -4,15 +4,6 @@ export class MainPage extends Component {
 	constructor(parent: HTMLElement) {
 		super(parent, 'section', ['main-page']);
 
-		const imgMaine = new Component(
-			this.root,
-			'img',
-			['imgMain'],
-			null,
-			['src', 'alt'],
-			['../assets/bg-main.png', 'main']
-		);
-
 		const titleContainerAll = new Component(this.root, 'div', ['main__cont']);
 
 		const titleContainerH1 = new Component(titleContainerAll.root, 'div', [
