d3ac3a6af02fb5947ffa0b65fdf819b0baa4b79e
elb00507 <elb00507@g.bstu.by>
2025-05-29 23:30:59 +0300

    feat: add file logicservice
    Добавлен файл логики

--- src/script.ts
+++ src/script.ts
@@ -3,12 +3,17 @@ import { Component } from './Abstract/Component';
 import { Footer } from './Common/Footer';
 import { Header } from './Common/Header';
 import { MainPage } from './Pages/MainPage';
+import { DBService } from './Services/DBService';
+import { LogicService } from './Services/LogicService';
 
 declare global {
 	interface Window {
 		app: App;
 	}
 }
+
+const dbService = new DBService();
+const logicService = new LogicService(dbService);
 
 class App {
 	constructor(parent: HTMLElement) {
